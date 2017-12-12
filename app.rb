ada1c6969ad419b53d1d12d18f09c922a0e85244
Michael Evans <michael@michaelgregoryevans.com>
2017-12-12 21:21:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,15 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+  post '/team' do
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @point_guard = params[:pg]
+    @shooting_guard = params[:sg]
+    @power_forward = params[:pf]
+    @small_forward = params[:sf]
+    @center = params[:c]
+    erb :team
+  end
+
 end
